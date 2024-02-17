e5a373db8b97543d9fa80860c76ba1167c716837
Igor Nogueira <117410107+IgorNogueiraPessoa@users.noreply.github.com>
2024-02-17 17:52:50 -0300

    Add files via upload

---
+++ 1Bimestre/ClasseModelagem-16-03/ExemploOO-02-05/src/main/java/com/mycompany/exemplooo/Principal.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.exemplooo;
+
+
+public class Principal {
+    
+    public static void main(String[] args)
+    {
+      //instanciação do objeto
+      Usuario usuario1 = new Usuario("Bartholomew","","","");
+      //chamada ao provarExistencia
+      usuario1.provarExistencia();
+    }
+    
+}
